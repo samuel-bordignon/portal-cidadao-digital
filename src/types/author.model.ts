4581497aedcc3d4b2501354372f92a04b39cbfdb
samue-bordignon <samuelborwie@gmail.com>
2026-06-08 22:31:10 -0300

    Adicionando a tipagem dos autores e inferindo os schemas zod

---
+++ src/types/author.model.ts
@@ -0,0 +1,12 @@
+import { createdAuthorSchema, UpdatedAuthorSchema } from '@/schemas/author.schema'
+import { z } from 'zod'
+
+export type Author = {
+  id: string
+  nome: string
+  email: string
+  role:  "admin" | "author"
+}
+
+export type CreateAuthorInput = z.infer<typeof createdAuthorSchema>
+export type UpdateAuthorInput = z.infer<typeof UpdatedAuthorSchema>
