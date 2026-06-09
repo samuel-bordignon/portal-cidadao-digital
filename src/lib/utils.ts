6df654c93feeb68f5824cb3cbe13e184a910381d
samue-bordignon <samuelborwie@gmail.com>
2026-06-08 23:39:18 -0300

    Adicionando função para gerar slugs a partir de texto

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const generateSlug = (text: string): string => {
+    return text.toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/[^a-z0-9]+/g, "-")
+        .replace(/^-|-$/g, "")
+}
