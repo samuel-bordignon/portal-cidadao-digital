d571e10b12a00251051f4b1e0c5a0fb1e165bb89
samue-bordignon <samuelborwie@gmail.com>
2026-06-11 10:19:00 -0300

    Refatorando a função updateCategory para utilizar hasCategory e simplificando a verificação de existência da categoria em deleteCategory e getCategoryById

--- src/service/category.service.ts
+++ src/service/category.service.ts
@@ -3,46 +3,42 @@ import { HttpError } from "@/lib/httpError"
 import * as categoryRepository from "@/repositories/category.repository"
 import { CreateCategoryInput, UpdateCategoryInput } from "@/types/category.model"
 
+const hasCategory = async (id: string) => {
+    const category = await categoryRepository.findById(id)
+    if (!category) {
+        throw new HttpError(404, "Autor não encontrado")
+    }
+    return category
+}
+
 export const createCategory = async (data: Pick<CreateCategoryInput, 'nome'>, author_id: string) => {
     const slug = generateSlug(data.nome)
 
     return await categoryRepository.create({ ...data, slug, author_id: author_id })
 }
+export const updateCategory = async (id: string, data: Omit<UpdateCategoryInput, 'slug'>, author_id: string, author_role: string) => {
+    const slug = generateSlug(data.nome)
 
-export const updateCategory = async (id: string, data: Pick<UpdateCategoryInput, 'nome'>, author_id: string, author_role: string) => {
-    const category = await categoryRepository.findById(id)
-
-    if (!category) {
-        throw new HttpError(404, "Categoria não encontrada")
-    }
+    const category = await hasCategory(id)
 
     if (category.author_id !== author_id && author_role !== 'admin') {
         throw new HttpError(403, "Sem permissão")
     }
-    const slug = generateSlug(data.nome)
+
     return await categoryRepository.update(id, { ...data, slug })
 }
-
 export const deleteCategory = async (id: string) => {
-    const category = await categoryRepository.findById(id)
-    if (!category) {
-        throw new HttpError(404, 'Categoria não encontrada')
-    }
-    await categoryRepository.remove(id)  
-}
+    await hasCategory(id)
 
+    await categoryRepository.remove(id)
+}
 export const getCategoryById = async (id: string) => {
-    const category = await categoryRepository.findById(id)
-    if (!category) {
-        throw new HttpError(404, 'Categoria não encontrada')
-    }
+    const category = await hasCategory(id)
     return category
 }
-
 export const getCategories = async () => {
     return await categoryRepository.findAll()
 }
-
 export const getCategoryBySlug = async (slug: string) => {
     const category = await categoryRepository.findBySlug(slug)
 
@@ -51,7 +47,6 @@ export const getCategoryBySlug = async (slug: string) => {
     }
     return category
 }
-
 export const getCategoriesByAuthor = async (author_id: string) => {
     const categories = categoryRepository.findByAuthor(author_id)
 
@@ -60,7 +55,11 @@ export const getCategoriesByAuthor = async (author_id: string) => {
     }
     return categories
 }
-
 export const getCategoriesWithPublishedPosts = async () => {
     return await categoryRepository.findWithPublishedPosts()
 }
+
+
+
+
+
