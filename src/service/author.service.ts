94093e68ead8f58fe41a7bebda92daa10cef4383
samue-bordignon <samuelborwie@gmail.com>
2026-06-11 10:21:27 -0300

    Refatorando a função hasAuthor para centralizar a verificação de existência do autor e simplificando as funções updateAuthor, deleteAuthor e getAuthorById

--- src/service/author.service.ts
+++ src/service/author.service.ts
@@ -3,6 +3,14 @@ import * as authorRepository from "@/repositories/author.repository"
 import { CreateAuthorInput, UpdateAuthorInput } from "@/types/author.model"
 import { createHash, randomBytes } from "crypto"
 
+const hasAuthor = async (id: string) => {
+    const category = await authorRepository.findById(id)
+    if (!category) {
+        throw new HttpError(404, "Autor não encontrado")
+    }
+    return category
+}
+
 export const createAuthor = async (data: Omit<CreateAuthorInput, "api_key_hash">) => {
     const rawKey = randomBytes(16).toString('hex')
     const hash = createHash("sha256").update(rawKey).digest("hex")
@@ -14,31 +22,21 @@ export const createAuthor = async (data: Omit<CreateAuthorInput, "api_key_hash">
 
     return { ...author, api_key: rawKey }
 }
-
 export const updateAuthor = async (id: string, data: UpdateAuthorInput) => {
-    const author = await authorRepository.findById(id)
-    if (!author) {
-        throw new HttpError(404, "Autor não encontrado")
-    }
+    await hasAuthor(id)
+
     return await authorRepository.update(id, data)
 }
-
 export const deleteAuthor = async (id: string) => {
-    const author = await authorRepository.findById(id)
-    if (!author) {
-        throw new HttpError(404, "Autor não encontrado")
-    }
+    await hasAuthor(id)
+
     await authorRepository.remove(id)
 }
-
 export const getAuthorById = async (id: string) => {
-    const author = await authorRepository.findById(id)
-    if (!author) {
-        throw new HttpError(404, "Autor não encontrado")
-    }
+    const author = await hasAuthor(id)
+
     return author
 }
-
 export const getAuthors = async () => {
     return await authorRepository.findAll()
 }
